f25ef48a3ae6aaad580790d664fb5bec72132c43
SaurabDey <saurab.jeet@gmail.com>
2019-04-04 09:26:11 +0530

    Get Post put Delete

--- src/main/java/com/restservice/example/ApplicationRestService/ApplicationRestServiceApplication.java
+++ src/main/java/com/restservice/example/ApplicationRestService/ApplicationRestServiceApplication.java
@@ -1,14 +1,20 @@
 package com.restservice.example.ApplicationRestService;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+//https://dzone.com/articles/spring-boot-restful-web-service-complete-example
 
 @SpringBootApplication
 public class ApplicationRestServiceApplication {
@@ -27,3 +33,50 @@ class MessageRestController {
         return rsp;
     }
 }
+
+@RestController
+class StudentRetrieveController {
+  @RequestMapping(method= {RequestMethod.GET}, produces= {MediaType.APPLICATION_JSON_VALUE}, value= {"/student/allstudent"})
+  @ResponseBody
+  public List<Student> getAllStudents() {
+  return StudentRegistration.getInstance().getStudentRecords();
+  }
+}
+
+@RestController
+class StudentRegistrationController {
+  @RequestMapping(method= {RequestMethod.POST}, produces= {MediaType.APPLICATION_JSON_VALUE}, value= {"/register/student"})
+  @ResponseBody
+  public StudentRegistrationReply registerStudent(@RequestBody Student student) {
+  System.out.println("In registerStudent");
+        StudentRegistrationReply stdregreply = new StudentRegistrationReply();           
+        StudentRegistration.getInstance().add(student);
+        //We are setting the below value just to reply a message back to the caller
+        stdregreply.setName(student.getName());
+        stdregreply.setAge(student.getAge());
+        stdregreply.setRegistrationNumber(student.getRegistrationNumber());
+        stdregreply.setRegistrationStatus("Successful");
+        return stdregreply;
+}
+}
+
+@RestController
+class StudentUpdateController {
+@RequestMapping(method= {RequestMethod.PUT}, produces= {MediaType.APPLICATION_JSON_VALUE}, value= {"/update/student"})
+@ResponseBody
+public String updateStudentRecord(@RequestBody Student stdn) {
+System.out.println("In updateStudentRecord");   
+    return StudentRegistration.getInstance().upDateStudent(stdn);
+}
+}
+
+@RestController
+class StudentDeleteController {
+@RequestMapping(method= {RequestMethod.DELETE}, produces= {MediaType.APPLICATION_JSON_VALUE}, value= {"/delete/student/{regdNum}"})
+@ResponseBody
+public String deleteStudentRecord(@PathVariable("regdNum") String regdNum) {
+System.out.println("In deleteStudentRecord");   
+    return StudentRegistration.getInstance().deleteStudent(regdNum);
+}
+}
+
